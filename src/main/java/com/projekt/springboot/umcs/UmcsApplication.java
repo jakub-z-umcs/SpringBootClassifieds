cc9909eeb4c910a9d73636bd39a419fcd0bd3b12
Adrian Zgrzebski <adrianski18@gmail.com>
2021-06-08 21:22:26 +0200

    Working endpoint

--- src/main/java/com/projekt/springboot/umcs/UmcsApplication.java
+++ src/main/java/com/projekt/springboot/umcs/UmcsApplication.java
@@ -2,12 +2,20 @@ package com.projekt.springboot.umcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UmcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmcsApplication.class, args);
+	}
+
+	@GetMapping
+	public String ads() {
+		return "Simple ad";
 	}
 
 }
