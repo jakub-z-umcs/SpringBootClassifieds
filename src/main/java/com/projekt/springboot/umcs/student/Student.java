9e77752f47071a883ff00f59a6cf90c10a574c8b
Adrian Zgrzebski <adrianski18@gmail.com>
2021-06-09 00:13:35 +0200

    Basic endpoint

--- src/main/java/com/projekt/springboot/umcs/student/Student.java
+++ src/main/java/com/projekt/springboot/umcs/student/Student.java
@@ -2,6 +2,7 @@ package com.projekt.springboot.umcs.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -21,21 +22,22 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
+        
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
+        
     }
 
     public Student() {
@@ -85,10 +87,6 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+        return Period.between(dob, LocalDate.now()).getYears();
     }
 }
