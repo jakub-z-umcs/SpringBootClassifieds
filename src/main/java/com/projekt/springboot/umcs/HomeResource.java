26e24717fb85b17b1354b31636cb56f11acc453d
Adrian Zgrzebski <adrianski18@gmail.com>
2021-07-08 10:56:39 +0200

    Change HTTP response status after success and failure login

--- src/main/java/com/projekt/springboot/umcs/HomeResource.java
+++ src/main/java/com/projekt/springboot/umcs/HomeResource.java
@@ -1,5 +1,7 @@
 package com.projekt.springboot.umcs;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeResource {
 
     @GetMapping("/")
-    public String home() {
-        return ("<h1>Welcome</h1>");
+    ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
     }
 }
